b6188f3b45f2c2bee27e36408cff5b0f16c32b1a
Antonio <tonyaguado99@gmail.com>
2020-10-15 20:51:17 +0100

    Errores corregidos y solucionados.

--- Documents/NetBeansProjects/Kata1/src/kata1/Person.java
+++ Documents/NetBeansProjects/Kata1/src/kata1/Person.java
@@ -9,22 +9,27 @@ import java.time.LocalDate;
 public class Person {
     private final String name;
     private final LocalDate birthdate;
-    
-    public Person (final String name, LocalDate birthdate) {
+
+    public Person(String name, LocalDate birthdate) {
         this.name = name;
-        this.birthdate  = birthdate;
+        this.birthdate = birthdate;
     }
     public String getName() {
         return name;
     }
+
     public LocalDate getBirthdate() {
         return birthdate;
     }
+
     public int getAge() {
         return toYears(LocalDate.now().toEpochDay() - birthdate.toEpochDay());
     }
-    private int toYears(long l) {
-        return 0;
+
+    private int toYears(long days) {
+        return (int)(days/365);
     }
+
+    
     
 }
